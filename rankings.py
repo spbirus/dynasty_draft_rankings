60d3028151a54cdf37ccbe32920311bde898116f
spbirus <burritophil@gmail.com>
2022-12-20 16:17:06 +0000

    Initial fantasy data

--- rankings.py
+++ rankings.py
@@ -1,1 +1,84 @@
+import requests
+import json 
+import matplotlib.pyplot as plt
 
+def getpath(nested_dict, value, prepath=()):
+    for k, v in nested_dict.items():
+        path = prepath + (k,)
+        if v == value: # found value
+            return path
+        elif hasattr(v, 'items'): # v is a dict
+            p = getpath(v, value, path) # recursive call
+            if p is not None:
+                return p
+
+SLEEPER_ID = 802628454850875392
+BASE_SLEEPER_URL = "https://api.sleeper.app/v1"
+HIDDEN_SLEEPER_URL = "https://api.sleeper.app"
+
+# retrieve all players
+res = requests.get(BASE_SLEEPER_URL + "/players/nfl")
+all_players = json.loads(res.text)
+
+# retrieve managers
+res = requests.get(BASE_SLEEPER_URL + "/league/{}/users".format(SLEEPER_ID))
+response = json.loads(res.text)
+
+users = []
+for r in response:
+    try:
+        users.append({
+            'user_id': r['user_id'],
+            'user_name': r['display_name'],
+            'team_name': r['metadata']['team_name']
+        })
+    except KeyError:
+        users.append({
+            'user_id': r['user_id'],
+            'user_name': r['display_name'],
+            'team_name': r['display_name']
+        })
+
+# get draft picks
+draft_2020_names = ["Jonathan Taylor","Clyde Edwards-Helaire","J.K. Dobbins","CeeDee Lamb","Cam Akers","Swift","Justin Jefferson","Jerry Jeudy","Henry Ruggs","Brandon Aiyuk","Jalen Reagor","Tee Higgins","Joe Burrow","Ke'Shawn Vaughn","Michael Pittman","Denzel Mims","AJ Dillon","Bryan Edwards","Zack Moss","Chase Claypool","Laviska Shenault","Tua Tagovailoa","Devin Duvernay","Anthony McFarland","Darrynton Evans","K.J. Hamler","Cole Kmet","Justin Herbert","Jack Doyle","Antonio Gibson","Van Jefferson","Gandy-Golden"]
+draft_2020_data = []
+draft_2021_names = ["Najee Harris","Ja'Marr Chase","Kyle Pitts","Travis Etienne","Javonte Williams","Trevor Lawrence","Jaylen Waddle","DeVonta Smith","Elijah Moore","Trey Sermon","Michael Carter","Trey Lance","Justin Fields","Terrace Marshall","Rashod Bateman","Rondale Moore","Joshua Palmer","Amon-Ra St. Brown","Kadarius Toney","Zach Wilson","Dyami Brown","Tutu Atwell","Kenneth Gainwell","Mac Jones","Chuba Hubbard","Pat Freiermuth","Rhamondre Stevenson","Nico Collins","","Anthony Schwartz","Amari Rodgers","Hunter Henry"]
+draft_2021_data = []
+draft_2022_names = ["Breece Hall","Kenneth Walker","Jameson Williams","Drake London","Chris Olave","Garrett Wilson","Jahan Dotson","Treylon Burks","Skyy Moore","George Pickens","James Cook","Christian Watson","Rachaad White","Isaiah Spiller","Kenny Pickett","Alec Pierce","David Bell","Jalen Tolbert","John Metchie","Dameon Pierce","Trey McBride","Brian Robinson","Tyquan Thornton","Khalil Shakir","Wan'Dale Robinson","Greg Dulcich","Malik Willis","Tyler Allgeier","Desmond Ridder","Romeo Doubs","Jelani Woods","Calvin Austin","Kyren Williams","Hassan Haskins","Tyrion Davis-Price","Daniel Bellinger","Sam Howell","Zamir White","Taysom Hill","Brock Purdy","Matt Corral","Brevin Jordan","Velus Jones","Pierre Strong","Justyn Ross","Nick Westbrook-Ikhine","","Gerald Everett"]
+draft_2022_data = []
+
+def get_draft_data(player_names, player_data):
+    years = [2022,2021,2020,2019]
+    idx = 1
+    for name in player_names:
+        player_id = getpath(all_players, name)[0]
+        total_fpts = 0
+        for year in years:
+            try:
+                res = requests.get(HIDDEN_SLEEPER_URL + "/stats/nfl/player/{}?season_type=regular&season={}".format(player_id, year))
+                response = json.loads(res.text)
+                total_fpts = total_fpts + response['stats']['pts_half_ppr']
+            except:
+                break
+
+        player_data.append({
+            'id': player_id,
+            'name': name,
+            'draft_pos': idx,
+            'total_fpts': total_fpts,
+            # TODO: add who player was drafted_by
+        })
+        idx = idx + 1
+
+get_draft_data(draft_2020_names, draft_2020_data)
+get_draft_data(draft_2021_names, draft_2021_data)
+get_draft_data(draft_2022_names, draft_2022_data)
+
+# plot the dat
+x = [1,2,3]
+y = [2,4,1]
+plt.plot(x, y)
+plt.xlabel('Draft Position')
+plt.ylabel('Fantasy Points')
+plt.title('Draft Position vs Fantasy Points')
+plt.show()
